e3f50ef88f4f2b3543d4f9fef69f6a2957f9a6c7
radioheadxx <holdenkolfild@inbox.ru>
2022-08-10 16:02:42 +0300

    final

--- src/main/java/org/example/App.java
+++ src/main/java/org/example/App.java
@@ -7,6 +7,8 @@ import org.springframework.http.HttpEntity;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.MediaType;
 
+import java.util.Arrays;
+
 public class App 
 {
     public static void main( String[] args )
@@ -14,13 +16,23 @@ public class App
         AnnotationConfigApplicationContext context = new AnnotationConfigApplicationContext(MyConfig.class);
 
         HttpHeaders headers = new HttpHeaders();
-        HttpEntity<User> entity = new HttpEntity<>(headers);
         headers.setContentType(MediaType.APPLICATION_JSON);
-
+        headers.setAccept(Arrays.asList(new MediaType[] { MediaType.APPLICATION_JSON }));
         Communication communication = context.getBean("communication", Communication.class);
 
-        String allUsers = communication.getAllUsers(entity);
-        System.out.println(allUsers);
+        String sessionID = communication.getAllUsers(headers);
+        headers.set("Cookie", sessionID);
+
+//      Сохранить пользователя с id = 3, name = James, lastName = Brown, age = на ваш выбор. В случае успеха вы получите первую часть кода.
+        User user = new User(3L, "James", "Brown", (byte) 27);
+        communication.saveUser(headers, user);
+
+//      Изменить пользователя с id = 3. Необходимо поменять name на Thomas, а lastName на Shelby. В случае успеха вы получите еще одну часть кода.
+        User user2 = new User(3L, "Thomas", "Shelby", (byte) 27);
+        communication.updateUser(headers, user2);
+
+//      Удалить пользователя с id = 3. В случае успеха вы получите последнюю часть кода.
+        communication.deleteUser(headers, 3);
     }
 }
 
--- src/main/java/org/example/Communication.java
+++ src/main/java/org/example/Communication.java
@@ -6,8 +6,6 @@ import org.springframework.http.*;
 import org.springframework.stereotype.Component;
 import org.springframework.web.client.RestTemplate;
 
-import java.util.Arrays;
-
 @Component
 public class Communication {
 
@@ -15,24 +13,28 @@ public class Communication {
     private RestTemplate restTemplate;
     private final String URL = "http://94.198.50.185:7081/api/users";
 
-
-    public String getAllUsers(HttpEntity<User> entity) {
-//        headers.setAccept(Arrays.asList(new MediaType[] { MediaType.APPLICATION_JSON }));
+    public String getAllUsers(HttpHeaders headers) {
+        HttpEntity<User> entity = new HttpEntity<>(headers);
         ResponseEntity<String> responseEntity = restTemplate.exchange(URL, HttpMethod.GET, entity, String.class);
-        return responseEntity.getBody();
+        return responseEntity.getHeaders().get("Set-Cookie").get(0);
     }
 
-    public void saveUser(User user) {
-        long id = user.getId();
-        if(id==0) {
-            ResponseEntity<String> responseEntity = restTemplate.postForEntity(URL, user, String.class);
-        } else {
-            restTemplate.put(URL, user);
-        }
+    public void saveUser(HttpHeaders headers, User user) {
+        HttpEntity<User> entity = new HttpEntity<>(user, headers);
+        ResponseEntity<String> responseEntity = restTemplate.exchange(URL, HttpMethod.POST, entity, String.class);
+        System.out.println(responseEntity.getBody());
     }
 
-    public void deleteUser(int id) {
-        restTemplate.delete(URL + "/" + id);
+    public void updateUser(HttpHeaders headers, User user) {
+        HttpEntity<User> entity = new HttpEntity<>(user, headers);
+        ResponseEntity<String> responseEntity = restTemplate.exchange(URL, HttpMethod.PUT, entity, String.class);
+        System.out.println(responseEntity.getBody());
+    }
+
+    public void deleteUser(HttpHeaders headers, int id) {
+        HttpEntity<User> entity = new HttpEntity<>(headers);
+        ResponseEntity<String> responseEntity = restTemplate.exchange(URL + "/" + id, HttpMethod.DELETE, entity, String.class);
+        System.out.println(responseEntity.getBody());
     }
 
 }
